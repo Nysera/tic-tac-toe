d5f1cd5a023fcb542ff18ddeee425b1536dccfb4
Nysera <73109965+Nysera@users.noreply.github.com>
2021-01-05 16:43:35 +1000

    Add logic for player1

--- assets/js/app.js
+++ assets/js/app.js
@@ -2,6 +2,20 @@ const dom = (function() {
     const cacheDom = {
         boardContainer: document.querySelector(".board")
     };
+    const squareSelector = function() {
+        return cacheDom.boardContainer.querySelectorAll(".square");
+    };
+    const setMarker = function(el, marker) {
+        const span = el.querySelector("span");
+        span.innerHTML = marker;
+        if (marker === "o") {
+            setTimeout(function(){
+                span.classList.add("visible");
+            }, 180);
+        } else {
+            span.classList.add("visible");
+        }
+    };
     const squareMarkup = function() {
         const square = document.createElement("div");
         square.classList.add("square");
@@ -17,16 +31,36 @@ const dom = (function() {
 
     // public scope
     return {
-        render
+        render,
+        squareSelector,
+        setMarker
     };
 })();
+
+const playerFactory = function(name, marker, type) {
+    const takeTurn = function() {
+        console.log({ name, marker, type });
+        if (name === "player1") {
+            const index = Array.from(event.currentTarget.parentNode.children).indexOf(event.currentTarget);
+            gameBoard.addMarker(index, marker);
+        }
+    };
+
+    return {
+        takeTurn
+    };
+};
 
 const gameBoard = (function() {
     let board = [];
     const square = {
         marker: ""
     };
-
+    const addMarker = function(index, marker) {
+        const getSquareDivFromIndex = Array.from(dom.squareSelector())[index];
+        board[index] = { marker }
+        dom.setMarker(getSquareDivFromIndex, marker);
+    };
     const initializeBoard = function() {
         for (let i = 0; i < 9; i++ ) {
             board.push(square);
@@ -34,5 +68,39 @@ const gameBoard = (function() {
         dom.render(board);
     };
 
-    initializeBoard();
+    // public scope
+    return {
+        initializeBoard,
+        addMarker
+    };
+})();
+
+const game = (function() {
+    const player1 = playerFactory("player1", "x", "human");
+    let player2 = playerFactory("player2", "o", "human");
+    let isPlayer1Turn = true;
+    let currentPlayer;
+
+    const startGame = function() {
+        gameBoard.initializeBoard();
+        setPlayer();
+        addSquareEventListener();
+    };
+    const setPlayer = function() {
+        isPlayer1Turn ? currentPlayer = player1 : currentPlayer = player2;
+    };
+
+    const addSquareEventListener = function() {
+        dom.squareSelector().forEach(function(square){
+            square.addEventListener("click", function(event){
+                if (event.currentTarget.textContent === "") {
+                    currentPlayer.takeTurn(event);
+                }
+            });
+        });
+    };
+
+
+
+    startGame();
 })();
