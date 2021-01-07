b40623b944f6c1cc14e6bc036b40db91fadb10ed
Nysera <73109965+Nysera@users.noreply.github.com>
2021-01-07 13:38:01 +1000

    Add logic for changing from 1 player to 2 player

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,6 +1,8 @@
 const dom = (function() {
     const cacheDom = {
-        boardContainer: document.querySelector(".board")
+        boardContainer: document.querySelector(".board"),
+        playerToggleBtn: document.querySelector("#player-toggle"),
+        player2ScoreContainer: document.querySelector("#computer")
     };
     const squareSelector = function() {
         return cacheDom.boardContainer.querySelectorAll(".square");
@@ -22,6 +24,9 @@ const dom = (function() {
             } else {
                 cacheDom.boardContainer.classList.remove("tie");
             }
+        } else {
+            cacheDom.boardContainer.classList.remove("winner");
+            cacheDom.boardContainer.classList.remove("tie");
         }
     };
     const setMarker = function(el, marker) {
@@ -54,6 +59,7 @@ const dom = (function() {
 
     // public scope
     return {
+        cacheDom,
         render,
         squareSelector,
         setMarker,
@@ -135,9 +141,11 @@ const game = (function() {
         gameBoard.initializeBoard();
         setPlayer();
         addSquareEventListener();
+        dom.cacheDom.playerToggleBtn.addEventListener("click", changePlayer);
     };
     const resetGame = function() {
         isPlayer1Turn = true;
+        dom.toggleWinnerClasses();
         startGame();
     };
     const setPlayer = function() {
@@ -145,6 +153,22 @@ const game = (function() {
     };
     const switchPlayer = function() {
         isPlayer1Turn = !isPlayer1Turn;
+    };
+    const changePlayer = function() {
+        if (player2.type === "computer") {
+            dom.cacheDom.playerToggleBtn.querySelector(".icon").innerHTML = `<svg version="1.1" xmlns="http://www.w3.org/2000/svg" width="30" height="28" viewBox="0 0 18 16"><path d="M12 12.041v-0.825c1.102-0.621 2-2.168 2-3.716 0-2.485 0-4.5-3-4.5s-3 2.015-3 4.5c0 1.548 0.898 3.095 2 3.716v0.825c-3.392 0.277-6 1.944-6 3.959h14c0-2.015-2.608-3.682-6-3.959z"></path><path d="M5.112 12.427c0.864-0.565 1.939-0.994 3.122-1.256-0.235-0.278-0.449-0.588-0.633-0.922-0.475-0.863-0.726-1.813-0.726-2.748 0-1.344 0-2.614 0.478-3.653 0.464-1.008 1.299-1.633 2.488-1.867-0.264-1.195-0.968-1.98-2.841-1.98-3 0-3 2.015-3 4.5 0 1.548 0.898 3.095 2 3.716v0.825c-3.392 0.277-6 1.944-6 3.959h4.359c0.227-0.202 0.478-0.393 0.753-0.573z"></path></svg>`;
+            dom.cacheDom.playerToggleBtn.querySelector(".value").innerHTML = "2P";
+            dom.cacheDom.player2ScoreContainer.querySelector(".title").innerHTML = "Player 2(<b>O</b>)"
+            player2 = playerFactory("player2", "o", "human");
+            resetGame();
+        } else {
+            dom.cacheDom.playerToggleBtn.querySelector(".icon").innerHTML = `<svg version="1.1" xmlns="http://www.w3.org/2000/svg" width="30" height="30" viewBox="0 0 16 16"><path d="M9 11.041v-0.825c1.102-0.621 2-2.168 2-3.716 0-2.485 0-4.5-3-4.5s-3 2.015-3 4.5c0 1.548 0.898 3.095 2 3.716v0.825c-3.392 0.277-6 1.944-6 3.959h14c0-2.015-2.608-3.682-6-3.959z"></path></svg>`;
+            dom.cacheDom.playerToggleBtn.querySelector(".value").innerHTML = "1P";
+            dom.cacheDom.player2ScoreContainer.querySelector(".title").innerHTML = "Computer(<b>O</b>)"
+            player2 = playerFactory("player2", "o", "computer");
+            resetGame();
+            dom.toggleWinnerClasses();
+        }
     };
     const addSquareEventListener = function() {
         dom.squareSelector().forEach(function(square){
